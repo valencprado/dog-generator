b322b6a9740c8aa93738b2ba839356fc3a852948
Valentina Prado <valenpschool@gmail.com>
2024-10-24 23:39:22 -0300

    feat: componente de navegação

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,22 +1,22 @@
-import {Menu} from 'antd'
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
+
 const items = [
-    {
-        label: 'Home',
-        key: 'home',
-    },
-    {
-        label: 'Raça',
-        key: 'breed',
-    },
-    {
-        label: 'Ver todos',
-        key: 'all'
-    }
-]
+  {
+    label: <Link to="/" style={{fontSize: '1.2rem'}}>Home</Link>,
+    key: "home",
+  },
+  {
+    label: <Link to="/breeds" style={{fontSize: '1.2rem'}}>Raças</Link>,
+    key: "breed",
+  },
+  {
+    label: <Link to="/about" style={{fontSize: '1.2rem'}}>Sobre</Link>,
+    key: "all",
+  },
+];
 const Navbar = () => {
-    return (
-    <Menu items={items} mode='horizontal'/>
-    )
-}
+  return <Menu items={items} mode="horizontal" theme="light" />;
+};
 
 export default Navbar;
